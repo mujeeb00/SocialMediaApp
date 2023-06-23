b56bc6313b3c7d97d8d2fa1a70282efd3becad0e
Mujeeb Ur rehman <113839359+mujeeb00@users.noreply.github.com>
2023-06-23 18:22:56 +0500

    Added component for Suggestion card using api

--- src/components/Suggestioncard.jsx
+++ src/components/Suggestioncard.jsx
@@ -1,43 +1,51 @@
-import React from 'react'
-import { useState } from 'react'
-import axios from 'axios'
-import './Suggestioncard.css'
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import "./Suggestioncard.css";
+import {HiPlus} from 'react-icons/hi'
 
 const Suggestioncard = () => {
+  const [data, setData] = useState([]);
 
-    const [data,setData]=useState([])
-
-    axios.get('https://api.github.com/users').then((res)=>{setData(res.data) }).catch((err)=>{console.log(err,'error')})
+  axios
+    .get("https://api.github.com/users")
+    .then((res) => {
+      setData(res.data);
+    })
+    .catch((err) => {
+      console.log(err, "error");
+    });
   return (
-    <div className='suggestion-container rounded mt-3'>
+    <div className="suggestion-container rounded mt-3">
       <div className="header">
-        <h5 className='text-center mb-4 p-2'>Who to follow</h5>
+        <h5 className="text-center mb-4 p-2">Who to follow</h5>
       </div>
       <div className="cardbody ps-1">
-
         {/* Map method */}
-        {
-          data.slice(5,10).map((user)=>(
-            <div className="row">
+        {data.slice(5, 10).map((user) => (
+          <div className="row">
             <div className="col-3">
-                <img className='rounded-circle' style={{width:'50px'}} src={user.avatar_url} alt="" />
+              <img
+                className="rounded-circle"
+                style={{ width: "50px" }}
+                src={user.avatar_url}
+                alt=""
+              />
             </div>
-            <div className="col-8">
-                {/* Name */}
-                <p>{user.login}</p>
-               
-
-                {/* title */}
-                <p>{user.type}</p>
+            <div className="col-5">
+             
+              {/* Name */}
+              <p className="fw-bold">{user.login}<br /> <p className="fw-light">{user.type}</p></p>
+              
             </div>
-        </div>
-          ))
-        }
-        
-
+            <div className="col-3">
+              <button className="addbtn btn btn-primary  btn-primary-soft icon-md rounded-circle ms-3 mt-2"><HiPlus/></button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Suggestioncard
+export default Suggestioncard;
