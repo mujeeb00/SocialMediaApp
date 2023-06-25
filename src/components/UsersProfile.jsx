5684caab237995d5376634d33df104e3d98f1c6a
Mujeeb Ur rehman <113839359+mujeeb00@users.noreply.github.com>
2023-06-25 11:26:06 +0500

    improved comment ui and functionality the comment will now come with user profile and his name and also added comment like and replies option

--- src/components/UsersProfile.jsx
+++ src/components/UsersProfile.jsx
@@ -38,43 +38,70 @@ const UsersProfile = ({ user}) => {
   return (
     <div className="row" key={user.id}>
       <div className="col-1 me-1 ps-3 mt-3">
-      <img
+        <img
           className="rounded-circle text-center"
-          style={{ width: '40px' }}
+          style={{ width: "40px" }}
           src={userimg}
           alt=""
         />
       </div>
-      
+
       <div className="col-10 mt-3">
-      
-      <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="Add comment..."
-          aria-label="Add comment..."
-          aria-describedby="basic-addon2"
-          value={comment}
-          onChange={handleCommentChange}
-        />
-        <Button variant="outline-secondary" id="button-addon2" onClick={handleAddComment}>
-        <BsFillSendFill/>
-        </Button>
-      </InputGroup>
-        
-       
+        <InputGroup className="mb-3">
+          <Form.Control
+            placeholder="Add comment..."
+            aria-label="Add comment..."
+            aria-describedby="basic-addon2"
+            value={comment}
+            onChange={handleCommentChange}
+          />
+          <Button
+            variant="outline-secondary"
+            id="button-addon2"
+            onClick={handleAddComment}
+          >
+            <BsFillSendFill />
+          </Button>
+        </InputGroup>
       </div>
       {comments.map((comment, index) => (
-        <div className='row' key={index} >
+        <div className="row" key={index}>
           <div className="col-1 ">
-          <img
-        className="rounded-circle text-center mt-3"
-        style={{ width: '35px' }}
-        src={userimg}
-        alt=""
-      /></div>
-      <div className="col-8">
-        <p className='mt-3 ms-1 bg-dark p-3 rounded' >&nbsp;&nbsp;{comment}</p>
-        </div></div>
+            <img
+              className="rounded-circle text-center mt-3"
+              style={{ width: "35px" }}
+              src={userimg}
+              alt=""
+            />
+          </div>
+          <div className="col-8">
+            <p className="mt-3 ms-1 bg-dark p-3 rounded fw-lighter fs-6">
+              <span>Lori furgeson</span> <br />
+              &nbsp;&nbsp;{comment}
+            </p>
+
+            <ul class="nav nav-divider  small">
+              <li class="nav-item">
+                <a class="nav-link text-secondary" href="#!">
+                  {" "}
+                  Like {Math.floor(Math.random() * 100)}
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-secondary" href="#!">
+                  {" "}
+                  Reply
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-secondary" href="#!">
+                  {" "}
+                  View {Math.floor(Math.random() * 10)} replies
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       ))}
     </div>
   );
