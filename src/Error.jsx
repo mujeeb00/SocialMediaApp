7f8f10dc965ba1a7680820b4f8e7250d84e875fd
Mujeeb Ur rehman <113839359+mujeeb00@users.noreply.github.com>
2023-06-26 23:45:44 +0500

    Added Error page

---
+++ src/Error.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import erimg from '../src/erimg.svg'
+
+const Error = () => {
+  return (
+    <div class="container d-flex justify-content-center">
+    <img className='img-fluid  ' style={{width:'600px'}} src={erimg} alt="" />
+    <br />
+    
+</div>
+  )
+}
+
+export default Error
