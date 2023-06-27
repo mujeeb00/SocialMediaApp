7adcd1126060a32789bafe293ea6803918b9c2d3
Mujeeb Ur rehman <113839359+mujeeb00@users.noreply.github.com>
2023-06-27 10:45:16 +0500

    added bootstrape classes to make it responsive and improved ui

--- src/components/Settings/Settingcard.jsx
+++ src/components/Settings/Settingcard.jsx
@@ -1,55 +1,51 @@
-import React from 'react';
-import './Setting.css'
-import {FaUserAlt} from 'react-icons/fa'
-import {BsFillBellFill} from 'react-icons/bs'
-
-
+import './Setting.css';
+import { FaUserAlt } from 'react-icons/fa';
+import { BsFillBellFill } from 'react-icons/bs';
 
 const Settingcard = () => {
   return (
-    <div className="card-body ">
+    <div className="card-bodySetting rounded">
       <ul className="nav nav-tabs flex-column border rounded p-2" role="tablist">
-      <li className="nav-item pt-3" role="presentation">
-          <a className="nav-link d-flex mb-0" href="#nav-setting-tab-2" data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">
-            <FaUserAlt/>
-            <span className='container' >Account</span>
+        <li className="nav-item pt-3" role="presentation">
+          <a className="nav-link d-flex mb-0" href="#nav-setting-tab-2" data-bs-toggle="tab" aria-selected="false" tabIndex="-1" role="tab">
+            <FaUserAlt />
+            <span className="ms-2">Account</span>
           </a>
         </li>
         <li className="nav-item" role="presentation">
-          <a className="nav-link d-flex mb-0" href="#nav-setting-tab-2" data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">
-          <BsFillBellFill/>
-            <span className='container'>Notification</span>
+          <a className="nav-link d-flex mb-0" href="#nav-setting-tab-2" data-bs-toggle="tab" aria-selected="false" tabIndex="-1" role="tab">
+            <BsFillBellFill />
+            <span className="ms-2">Notification</span>
           </a>
         </li>
         <li className="nav-item" role="presentation">
-          <a className="nav-link d-flex mb-0" href="#nav-setting-tab-3" data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">
-            <BsFillBellFill/>
-            <span className='container' >Privacy and safety</span>
+          <a className="nav-link d-flex mb-0" href="#nav-setting-tab-3" data-bs-toggle="tab" aria-selected="false" tabIndex="-1" role="tab">
+            <BsFillBellFill />
+            <span className="ms-2">Privacy and safety</span>
           </a>
         </li>
         <li className="nav-item" role="presentation">
-          <a className="nav-link d-flex mb-0" href="#nav-setting-tab-4" data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">
-            <BsFillBellFill/>
-            <span className='container' >Communications</span>
+          <a className="nav-link d-flex mb-0" href="#nav-setting-tab-4" data-bs-toggle="tab" aria-selected="false" tabIndex="-1" role="tab">
+            <BsFillBellFill />
+            <span className="ms-2">Communications</span>
           </a>
         </li>
         <li className="nav-item" role="presentation">
-          <a className="nav-link d-flex mb-0" href="#nav-setting-tab-5" data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">
-            <BsFillBellFill/>
-            <span className='container' >Messaging</span>
+          <a className="nav-link d-flex mb-0" href="#nav-setting-tab-5" data-bs-toggle="tab" aria-selected="false" tabIndex="-1" role="tab">
+            <BsFillBellFill />
+            <span className="ms-2">Messaging</span>
           </a>
         </li>
         <li className="nav-item" role="presentation">
-          <a className="nav-link d-flex mb-0" href="#nav-setting-tab-6" data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">
-            <BsFillBellFill/>
-            <span className='container' >Close account</span>
+          <a className="nav-link d-flex mb-0" href="#nav-setting-tab-6" data-bs-toggle="tab" aria-selected="false" tabIndex="-1" role="tab">
+            <BsFillBellFill />
+            <span className="ms-2">Close account</span>
           </a>
         </li>
-       
-        <li className="nav-item  d-flex" role="presentation">
-             <hr/>
-          <a className="nav-link d-flex mt-2 container justify-content-center align-item-center" href="#nav-setting-tab-6" data-bs-toggle="tab" aria-selected="false" tabindex="-1" role="tab">
-           view profile
+        <li className="nav-item d-flex" role="presentation">
+          <hr />
+          <a className="nav-link text-center container me-4" href="#nav-setting-tab-6" data-bs-toggle="tab" aria-selected="false" tabIndex="-1" role="tab">
+            View profile
           </a>
         </li>
       </ul>
