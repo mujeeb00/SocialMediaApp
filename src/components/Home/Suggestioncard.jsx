381497fa70141e74f807a997ad1c831dc157d297
Mujeeb Ur rehman <113839359+mujeeb00@users.noreply.github.com>
2023-06-27 11:52:16 +0500

    updated Suggestion card component

---
+++ src/components/Home/Suggestioncard.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Suggestioncard.css";
+import {HiPlus} from 'react-icons/hi'
+import Usersdata from "../hooks/Usersdata";
+
+const Suggestioncard = () => {
+  const users = Usersdata();
+
+  
+  return (
+    <div className="suggestion-container rounded mt-3">
+      <div className="header">
+        <h5 className="text-center mb-4 p-2">Who to follow</h5>
+      </div>
+      <div className="cardbody ps-1">
+        {/* Map method */}
+        {users.slice(5, 10).map((user) => (
+          <div className="row">
+            <div className="col-3">
+              <img
+                className="rounded-circle"
+                style={{ width: "50px" }}
+                src={user.avatar_url}
+                alt=""
+              />
+            </div>
+            <div className="col-5">
+             
+              {/* Name */}
+
+              
+              <p className="fw-bold">{user.login}<br /> <p className="fw-light">{user.type}</p></p>
+              
+            </div>
+            <div className="col-3">
+              <button className="addbtn btn btn-primary  btn-primary-soft icon-md rounded-circle ms-3 mt-2"><HiPlus/></button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Suggestioncard;
