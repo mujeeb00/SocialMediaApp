fdb42e634a9fe120b75237ed13283e3f185589d4
Mujeeb Ur rehman <113839359+mujeeb00@users.noreply.github.com>
2023-07-02 19:23:25 +0500

    Fixed collapse issue

--- src/components/Home/UsersProfile.jsx
+++ src/components/Home/UsersProfile.jsx
@@ -1,29 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { Spinner } from 'react-bootstrap';
-import {BsFillSendFill} from 'react-icons/bs'
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Button from 'react-bootstrap/Button';
-import userimg from './Images/user.jpg'
+import React, { useState, useEffect } from "react";
+import { Spinner } from "react-bootstrap";
+import { BsFillSendFill } from "react-icons/bs";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import userimg from "./Images/user.jpg";
 
-const UsersProfile = ({ user}) => {
-  const [comment, setComment] = useState('');
+const UsersProfile = ({ user }) => {
+  const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
-
-
 
   const handleCommentChange = (event) => {
     setComment(event.target.value);
   };
 
   const handleAddComment = () => {
-    if (comment.trim() !== '') {
+    if (comment.trim() !== "") {
       setComments([...comments, comment]);
-      setComment('');
+      setComment("");
     }
   };
-
-  
 
   if (!user) {
     // Show a loader while data is being fetched
@@ -33,33 +29,36 @@ const UsersProfile = ({ user}) => {
   // Render the user profile component
   return (
     <div className="row" key={user.id}>
-      <div className="col-1 me-1 ps-3 mt-3">
-        <img
-          className="rounded-circle text-center"
-          style={{ width: "40px" }}
-          src={userimg}
-          alt=""
-        />
+      <div className="d-flex justify-content-start mt-2">
+        <div className="">
+          <img
+            className="rounded-circle text-center"
+            style={{ width: "40px" }}
+            src={userimg}
+            alt=""
+          />
+        </div>
+
+        <div className="ms-2">
+          <InputGroup className="mb-3">
+            <Form.Control
+              placeholder="Add comment..."
+              aria-label="Add comment..."
+              aria-describedby="basic-addon2"
+              value={comment}
+              onChange={handleCommentChange}
+            />
+            <Button
+              variant="outline-secondary"
+              id="button-addon2"
+              onClick={handleAddComment}
+            >
+              <BsFillSendFill />
+            </Button>
+          </InputGroup>
+        </div>
       </div>
 
-      <div className="col-10 mt-3">
-        <InputGroup className="mb-3">
-          <Form.Control
-            placeholder="Add comment..."
-            aria-label="Add comment..."
-            aria-describedby="basic-addon2"
-            value={comment}
-            onChange={handleCommentChange}
-          />
-          <Button
-            variant="outline-secondary"
-            id="button-addon2"
-            onClick={handleAddComment}
-          >
-            <BsFillSendFill />
-          </Button>
-        </InputGroup>
-      </div>
       {comments.map((comment, index) => (
         <div className="row" key={index}>
           <div className="col-1 ">
